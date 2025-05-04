c99f28f20a0263e50121f747fb5f03d9ebef891e
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2025-05-04 20:24:29 +0200

    feature: - added project files

---
+++ Dama/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Dama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.title2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.buttonPlay.Location = new System.Drawing.Point(159, 273);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(240, 75);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "GIOCA";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.playGame);
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F);
+            this.title.Location = new System.Drawing.Point(84, 52);
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            this.title.Size = new System.Drawing.Size(419, 128);
+            this.title.TabIndex = 1;
+            this.title.Text = "Dama";
+            this.title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.title2);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Controls.Add(this.buttonPlay);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 587);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(451, 526);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Giocatore 2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(318, 526);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Giocatore 1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ImageLocation = "pedina nera.png";
+            this.pictureBox2.Location = new System.Drawing.Point(451, 425);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "pedina bianca.png";
+            this.pictureBox1.Location = new System.Drawing.Point(318, 425);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 487);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(245, 29);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Gioca contro il Computer";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.cambioModalita);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(12, 425);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 29);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "1 VS 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.cambioModalita);
+            // 
+            // title2
+            // 
+            this.title2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.title2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.title2.Location = new System.Drawing.Point(108, 186);
+            this.title2.Name = "title2";
+            this.title2.ReadOnly = true;
+            this.title2.Size = new System.Drawing.Size(359, 23);
+            this.title2.TabIndex = 2;
+            this.title2.Text = "Sviluppato da Marroffino Antonio";
+            this.title2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 580);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonPlay;
+        public System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox title2;
+        public System.Windows.Forms.CheckBox checkBox2;
+        public System.Windows.Forms.CheckBox checkBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Dama/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dama
+{
+
+    public partial class Form1 : Form
+    {
+        const int RIGHE = 8;
+        const int COLONNE = 8;
+        public PanelRC[,] tabella = new PanelRC[RIGHE, COLONNE];
+        int tempR;
+        int tempC;
+        bool turnoGiocatore1 = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void playGame(object sender, EventArgs e)
+        {
+            title.Visible = false;
+            title2.Visible = false;
+            buttonPlay.Visible = false;
+            checkBox1.Visible = false;
+            checkBox2.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+
+
+            bool pedina = true;
+            creaTabella(pedina);
+        }
+        private void creaTabella(bool pedina)
+        {
+            int startR = 0;
+            int startC = 0;
+            for (int r = 0; r < RIGHE; r++)
+            {
+                for (int c = 0; c < COLONNE; c++)
+                {
+                    tabella[r, c] = new PanelRC();
+                    if ((r + c) % 2 == 0)
+                    {
+                        tabella[r, c].BackColor = Color.Black;
+                        if (r < 3)
+                        {
+                            tabella[r, c].pedina = new Pedina();
+                            if (pedina)
+                            {
+                                tabella[r, c].pedina.Image = Image.FromFile("pedina nera.png");
+                                tabella[r, c].pedina.colore = Color.Black;
+                            }
+
+                            else
+                            {
+                                tabella[r, c].pedina.Image = Image.FromFile("pedina bianca.png");
+                                tabella[r, c].pedina.colore = Color.White;
+                            }
+
+
+                            tabella[r, c].pedina.SizeMode = PictureBoxSizeMode.AutoSize;
+                            tabella[r, c].pedinaPresente = true;
+                            tabella[r, c].pedina.r = r;
+                            tabella[r, c].pedina.c = c;
+
+                            tabella[r, c].pedina.Click += new System.EventHandler(this.mostraMosse);
+
+                            this.tabella[r, c].Controls.Add(tabella[r, c].pedina);
+                        }
+                        else if (r > 4)
+                        {
+                            tabella[r, c].pedina = new Pedina();
+                            if (pedina)
+                            {
+                                tabella[r, c].pedina.Image = Image.FromFile("pedina bianca.png");
+                                tabella[r, c].pedina.colore = Color.White;
+                            }
+
+                            else
+                            {
+                                tabella[r, c].pedina.Image = Image.FromFile("pedina nera.png");
+                                tabella[r, c].pedina.colore = Color.Black;
+                            }
+
+                            tabella[r, c].pedina.SizeMode = PictureBoxSizeMode.AutoSize;
+                            tabella[r, c].pedinaPresente = true;
+                            tabella[r, c].pedina.r = r;
+                            tabella[r, c].pedina.c = c;
+
+                            tabella[r, c].pedina.Click += new System.EventHandler(this.mostraMosse);
+
+
+                            this.tabella[r, c].Controls.Add(tabella[r, c].pedina);
+
+                        }
+                    }
+                    else
+                        tabella[r, c].BackColor = Color.White;
+
+
+
+                    tabella[r, c].r = r;
+                    tabella[r, c].c = c;
+
+                    tabella[r, c].Width = 60;
+                    tabella[r, c].Height = 60;
+                    startC = r * tabella[r, c].Width + 50;
+                    startR = c * tabella[r, c].Width + 50;
+                    tabella[r, c].Location = new System.Drawing.Point(startR, startC);
+
+
+                    tabella[r, c].colore = 0;
+
+                    this.panel1.Controls.Add(tabella[r, c]);
+
+                }
+            }
+        }
+        private void mostraMosse(object sender, EventArgs e)
+        {
+            Pedina pedina = ((Pedina)sender);
+            PanelRC cella = tabella[pedina.r, pedina.c];
+
+            tempR = cella.r;
+            tempC = cella.c;
+
+            cancellaMosse();
+
+
+            if (turnoGiocatore1 == true || pedina.damone)
+            {
+                if (tabella[cella.r, cella.c].pedina.colore == Color.White)
+                {
+                    if (cella.r > 0)
+                    {
+                        if (cella.c > 0)
+                        {
+                            if (tabella[cella.r - 1, cella.c - 1].pedina == null)
+                            {
+                                tabella[cella.r - 1, cella.c - 1].BackColor = Color.Green;
+                                tabella[cella.r - 1, cella.c - 1].Click += new System.EventHandler(this.posizionaPedina);
+                            }
+                            else if (tabella[cella.r - 1, cella.c - 1].pedina.colore == Color.Black)
+                            {
+                                if (cella.r > 1)
+                                {
+                                    if (cella.c > 1)
+                                    {
+                                        if (tabella[cella.r - 2, cella.c - 2].pedina == null)
+                                        {
+                                            tabella[cella.r - 1, cella.c - 1].pedina.pedinaMangiata = true;
+                                            tabella[cella.r - 2, cella.c - 2].BackColor = Color.Green;
+                                            tabella[cella.r - 2, cella.c - 2].Click += new System.EventHandler(this.posizionaPedina);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (cella.c < COLONNE - 1)
+                        {
+                            if (tabella[cella.r - 1, cella.c + 1].pedina == null)
+                            {
+                                tabella[cella.r - 1, cella.c + 1].BackColor = Color.Green;
+                                tabella[cella.r - 1, cella.c + 1].Click += new System.EventHandler(this.posizionaPedina);
+                            }
+                            else if (tabella[cella.r - 1, cella.c + 1].pedina.colore == Color.Black)
+                            {
+                                if (cella.r > 1)
+                                {
+                                    if (cella.c < COLONNE - 2)
+                                    {
+                                        if (tabella[cella.r - 2, cella.c + 2].pedina == null)
+                                        {
+                                            tabella[cella.r - 1, cella.c + 1].pedina.pedinaMangiata = true;
+                                            tabella[cella.r - 2, cella.c + 2].BackColor = Color.Green;
+                                            tabella[cella.r - 2, cella.c + 2].Click += new System.EventHandler(this.posizionaPedina);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+
+            if (turnoGiocatore1 == false || pedina.damone)
+            {
+                if (tabella[cella.r, cella.c].pedina.colore == Color.Black)
+                {
+                    if (cella.r < RIGHE)
+                    {
+                        if (cella.c > 0)
+                        {
+                            if (tabella[cella.r + 1, cella.c - 1].pedina == null)
+                            {
+                                tabella[cella.r + 1, cella.c - 1].BackColor = Color.Green;
+                                tabella[cella.r + 1, cella.c - 1].Click += new System.EventHandler(this.posizionaPedina);
+                            }
+                            else if (tabella[cella.r + 1, cella.c - 1].pedina.colore == Color.White)
+                            {
+                                if (cella.r < RIGHE - 1)
+                                {
+                                    if (cella.c > 1)
+                                    {
+                                        if (tabella[cella.r + 2, cella.c - 2].pedina == null)
+                                        {
+                                            tabella[cella.r + 1, cella.c - 1].pedina.pedinaMangiata = true;
+                                            tabella[cella.r + 2, cella.c - 2].BackColor = Color.Green;
+                                            tabella[cella.r + 2, cella.c - 2].Click += new System.EventHandler(this.posizionaPedina);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (cella.c < COLONNE - 1)
+                        {
+                            if (tabella[cella.r + 1, cella.c + 1].pedina == null)
+                            {
+                                tabella[cella.r + 1, cella.c + 1].BackColor = Color.Green;
+                                tabella[cella.r + 1, cella.c + 1].Click += new System.EventHandler(this.posizionaPedina);
+                            }
+                            else if (tabella[cella.r + 1, cella.c + 1].pedina.colore == Color.White)
+                            {
+                                if (cella.r < RIGHE - 1)
+                                {
+                                    if (cella.c < COLONNE - 2)
+                                    {
+                                        if (tabella[cella.r + 2, cella.c + 2].pedina == null)
+                                        {
+                                            tabella[cella.r + 1, cella.c + 1].pedina.pedinaMangiata = true;
+                                            tabella[cella.r + 2, cella.c + 2].BackColor = Color.Green;
+                                            tabella[cella.r + 2, cella.c + 2].Click += new System.EventHandler(this.posizionaPedina);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        private void cancellaMosse()
+        {
+            for (int r = 0; r < RIGHE; r++)
+            {
+                for (int c = 0; c < COLONNE; c++)
+                {
+                    if (tabella[r, c].BackColor == Color.Green)
+                    {
+                        tabella[r, c].BackColor = Color.Black;
+                        tabella[r, c].Click -= new System.EventHandler(this.posizionaPedina);
+                    }
+                }
+            }
+        }
+        private void posizionaPedina(object sender, EventArgs e)
+        {
+            PanelRC cella = ((PanelRC)sender);
+            int differenza = 0;
+            int riga = 0, colonna = 0;
+            cancellaMosse();
+
+            trovaPedinaMangiata(ref riga, ref colonna);
+
+            if (cella.r < tempR)
+                differenza = tempR - cella.r;
+            else
+                differenza = cella.r - tempR;
+
+            if (differenza == 2)
+                pedinaMangiata(riga, colonna);
+            else
+                tabella[riga, colonna].pedina.pedinaMangiata = false;
+
+            cella.pedina = tabella[tempR, tempC].pedina;
+            cella.pedina.r = cella.r;
+            cella.pedina.c = cella.c;
+
+
+            cella.Controls.Add(tabella[tempR, tempC].pedina);
+            tabella[tempR, tempC].pedina = null;
+
+
+            if (turnoGiocatore1)
+                turnoGiocatore1 = false;
+            else
+                turnoGiocatore1 = true;
+
+            controllaDamone(cella);
+            
+            if (checkBox2.Checked)
+                mossaCPU();
+        }
+        private void trovaPedinaMangiata(ref int riga, ref int colonna)
+        {
+            for (int r = 0; r < RIGHE; r++)
+            {
+                for (int c = 0; c < COLONNE; c++)
+                {
+                    if (tabella[r, c].pedina != null)
+                        if (tabella[r, c].pedina.pedinaMangiata)
+                        {
+                            riga = tabella[r, c].r;
+                            colonna = tabella[r, c].c;
+                        }
+                }
+            }
+
+        }
+        private void pedinaMangiata(int riga, int colonna)
+        {
+            tabella[riga, colonna].pedina = null;
+            tabella[riga, colonna].Controls.Clear();
+        }
+        private void cambioModalita(object sender, EventArgs e)
+        {
+
+            if ((sender as CheckBox).Name == "checkBox1")
+            {
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = true;
+                checkBox1.Checked = true;
+                checkBox2.Checked = false;
+
+            }
+            else if ((sender as CheckBox).Name == "checkBox2")
+            {
+                checkBox2.Enabled = false;
+                checkBox1.Enabled = true;
+                checkBox2.Checked = true;
+                checkBox1.Checked = false;
+            }
+
+
+
+        }
+
+        private void controllaDamone(PanelRC cella)
+        {
+            if (cella.r == 0 || cella.r == RIGHE - 1)
+                cella.pedina.damone = true;
+
+        }
+        private void mossaCPU()
+        {
+
+        }
+    }
+
+    public class PanelRC : Panel
+    {
+        public Pedina pedina = null;
+        public bool pedinaPresente;
+        public int colore; //0 bianco, 1 nero
+        public int r;
+        public int c;
+    }
+
+    public class Pedina : PictureBox
+    {
+        public int r;
+        public int c;
+        public Color colore = Color.Red;
+        public bool damone = false;
+        public bool pedinaMangiata = false;
+    }
+}
